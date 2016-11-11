0a15a43fa2a5cce39028e9a2f889bebbf44a420a
peter.wang <peter.wang@DESKTOP-0015.mc.com>
2016-11-11 16:44:52 +0800

    github首页添加工具页面撒

---
+++ tools/js/toolbox.js
@@ -0,0 +1,8 @@
+var toollist = [
+	{name:'人民币大写转换', uri:'tools/rmbupper.html', category:'工具'},
+	{name:'计算器', uri:'tools/calculator.html', category:'工具'},
+	{name:'房贷计算器', uri:'tools/houseloancalc.html', category:'工具'},
+	{name:'身份证识别', uri:'tools/idrecoginize.html', category:'工具'},
+	{name:'发票报销工具', uri:'tools/invoicebaoxiao.html', category:'工具'}
+	
+];

fdc0d05751a51a36d0fa153a04a40f20c836f38e
peter.wang <peter.wang@DESKTOP-0015.mc.com>
2016-12-19 15:18:14 +0800

    !  增加上传文件转base64的工具

--- tools/js/toolbox.js
+++ tools/js/toolbox.js
@@ -3,6 +3,6 @@ var toollist = [
 	{name:'计算器', uri:'tools/calculator.html', category:'工具'},
 	{name:'房贷计算器', uri:'tools/houseloancalc.html', category:'工具'},
 	{name:'身份证识别', uri:'tools/idrecoginize.html', category:'工具'},
-	{name:'发票报销工具', uri:'tools/invoicebaoxiao.html', category:'工具'}
-	
+	{name:'发票报销工具', uri:'tools/invoicebaoxiao.html', category:'工具'},
+	{name:'文件base64工具', uri:'tools/filebase64.html', category:'工具'}
 ];
